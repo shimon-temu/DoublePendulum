9f14bd87a19a33af63fc8d482a03a02139292bcf
shimon-temu <tsukuba2011165@gmail.com>
2022-07-09 06:34:04 +0900

    Modify the result of transforming Euler-Lagrange equations.

--- DoublePendulum.m
+++ DoublePendulum.m
@@ -5,22 +5,26 @@ m1 = 2;
 m2 = 1;
 g = 9.80;
 theta1 = 2*pi/3;
-theta2 = 5*pi/6;
+theta2 = pi/2;
 omega1 = 0;
 omega2 = 0;
-tspan = linspace(0, 20, 1000000);
+tspan = [0 100];
 u0 = [theta1 theta2 omega1 omega2];
+options = odeset('RelTol',1e-8,'AbsTol',1e-10);
 
 % Compute answer of ODE. 
-[t, u] = ode23s(@(t, u) odefunc(t, u, l1, l2, m1, m2, g), tspan, u0);
+[t, u] = ode23s(@(t, u) odefunc(t, u, l1, l2, m1, m2, g), tspan, u0, options);
 x12 = 2*l1*sin(u(:, 1)) + 2*l2*sin(u(:, 2));
 y12 = -2*l1*cos(u(:, 1)) - 2*l2*cos(u(:, 2));
+vx12 = 2 * l1 * u(:, 2).*cos( u(:, 1) ) + l2 * u(:, 4).*cos( u(:, 2) );
+vy12 = 2 * l1 * u(:, 2).*sin( u(:, 1) ) + l2 * u(:, 4).*sin( u(:, 2) );
 
 % Compute answer of ODE when the value of l2 is changed. 
 l2 = l2 + 0.0000001;
-[t, u] = ode23s(@(t, y) odefunc(t, y, l1, l2, m1, m2, g), tspan, y0);
+[t, u] = ode23s(@(t, y) odefunc(t, y, l1, l2, m1, m2, g), tspan, u0, options);
 x22 = 2*l1*sin(u(:, 1)) + 2*l2*sin(u(:, 2));
 y22 = -2*l1*cos(u(:, 1)) - 2*l2*cos(u(:, 2));
+
 
 % Plot trajectories of double pendulums. 
 f1 = figure;
@@ -28,5 +32,4 @@ hold on;
 plot(x12, y12);
 plot(x22, y22);
 pbaspect([1 1 1]);
-legend('double pendulum'); 
 hold off;
--- odefunc.m
+++ odefunc.m
@@ -3,10 +3,9 @@ function dydt = odefunc(t, y, l1, l2, m1, m2, g)
     theta2 = y(2);
     omega1 = y(3);
     omega2 = y(4);
-    myu = m1 / m2;
     dydt = zeros(4, 1);
     dydt(1) = omega1;
     dydt(2) = omega2;
-    dydt(3) = ( -2 * l2 * omega2^2 * sin(theta1 - theta2) - (2+myu)*g*sin(theta1) + 2*( g*sin(theta2) - 2 * l1 * omega1^2 * sin(theta1-theta2) ) * cos(theta1-theta2) ) / (l1 * (myu + 4 * (sin(theta1-theta2))^2 ));
-    dydt(4) = ( (4+myu) * (2 * l1 * omega1^2 * sin(theta1-theta2) -g * sin(theta2)) + 2 * ( (2+myu)*g*sin(theta1) + 2*l2*omega2^2*sin(theta2) ) * cos(theta1-theta2) ) / (l2 * ( myu + 4 * (sin(theta1-theta2)^2) ));
+    dydt(3) = ( (l2^2) * ( -2*m2*l1*l2*(omega2^2)*sin(theta1 - theta2) - (m1 + 2*m2)*g*l1*sin(theta1) ) + ( -1 * m2 * 2*l1*l2*cos(theta1-theta2) ) * ( 2*l1*l2*(omega1^2)*sin(theta1 -theta2) - g*l2*sin(theta2) ) ) / ( (m1 + 4*m2) * (l1^2) * (l2^2) - m2*( 4*(l1^2)*(l2^2)*(cos(theta1 - theta2))^2 ) );
+    dydt(4) = ( (-1 * 2*l1*l2*cos(theta1-theta2) ) * ( -2*m2*l1*l2*(omega2^2)*sin(theta1 - theta2) - (m1 + 2*m2)*g*l1*sin(theta1) ) + ( (m1 + 4*m2) * (l1)^2) * ( 2*l1*l2*(omega1^2)*sin(theta1 -theta2) - g*l2*sin(theta2) )) / ( (m1 + 4*m2) * (l1^2) * (l2^2) - m2*( ( 4*(l1^2)*(l2^2)*(cos(theta1 - theta2))^2 ) ) );
 end
